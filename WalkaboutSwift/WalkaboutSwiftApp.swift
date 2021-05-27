82046fd9efc1b96fe84f6588b1b15cf85f025c14
Kyle Warren <qarren@gmail.com>
2021-05-27 15:21:55 -0700

    Initial Commit

---
+++ WalkaboutSwift/WalkaboutSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkaboutSwiftApp.swift
+//  WalkaboutSwift
+//
+//  Created by Kyle Warren on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WalkaboutSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
